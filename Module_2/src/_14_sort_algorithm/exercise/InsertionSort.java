9bda4411c0ffcd0a4f3fed4c43e8352b7f1bb70d
unknown <ndanh9698@gmail.com>
2021-06-23 17:35:55 +0700

    update

--- Module_2/src/_14_sort_algorithm/exercise/InsertionSort.java
+++ Module_2/src/_14_sort_algorithm/exercise/InsertionSort.java
@@ -1,5 +1,6 @@
 package _14_sort_algorithm.exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InsertionSort {
@@ -12,7 +13,10 @@ public class InsertionSort {
         for (int i=0;i<size;i++){
             array[i] = scanner.nextInt();
         }
-
+        System.out.println("Array original: ");
+        System.out.println(Arrays.toString(array));
+        System.out.println("----Sort-----");
+        sort1(array);
     }
 
     static void displayArray(int[] array){
@@ -21,8 +25,19 @@ public class InsertionSort {
         }
     }
 
-    static void sort(int[] array){
-        
+    static void sort1(int[] array){
+        int index = 0;
+        int temp = 0;
+        for(int i = 1; i < array.length; i++){
+            temp = array[i];
+            index = i;
+            while(index > 0 && temp < array[index-1]){
+                array[index] = array[index-1];
+                index--;
+            }
+            array[index] = temp;
+        }
+        displayArray(array);
     }
 
 }
